f2edc3465ce1320090ed59d7948a68db4febba66
Pedro Henrique <pedro_henrique_dsr@hotmail.com>
2023-05-19 22:39:49 -0300

    alguma coisa foi alterada

--- OpcoesIniciais.Designer.cs
+++ OpcoesIniciais.Designer.cs
@@ -28,23 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(421, 132);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(360, 164);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // OpcoesIniciais
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
             this.Name = "OpcoesIniciais";
             this.Size = new System.Drawing.Size(1172, 537);
             this.ResumeLayout(false);
@@ -52,7 +41,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
